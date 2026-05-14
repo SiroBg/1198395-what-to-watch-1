5cb59ac4ee383df3188275249d21de89af334620
Boris <borisusik@yandex.ru>
2026-05-14 16:09:13 +0300

    Добавляет контроллеры и роуты

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\FilmController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/user', [UserController::class, 'show']);
+Route::patch('/user', [UserController::class, 'update']);
+Route::post('/logout', [UserController::class, 'logout']);
+
+Route::get('/films', [FilmController::class, 'index']);
+Route::get('/films/{id}', [FilmController::class, 'show']);
+Route::get('/films/{id}/similar', [FilmController::class, 'similar']);
+
+Route::get('/genres', [GenreController::class, 'index']);
+Route::patch('/genres/{genre}', [GenreController::class, 'update']);
+
+Route::get('/favorite', [FavoriteController::class, 'index']);
+Route::post('/films/{id}/favorite', [FavoriteController::class, 'store']);
+Route::delete('/films/{id}/favorite', [FavoriteController::class, 'destroy']);
+
+Route::get('/comments/{id}', [CommentController::class, 'show']);
+Route::post('/comments/{id}', [CommentController::class, 'show']);
+Route::patch('/comments/{comment}', [CommentController::class, 'update']);
+Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
