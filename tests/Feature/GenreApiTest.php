cebb4f2e05b34b686daa1bb1dcd40ed672dfed0f
Boris <borisusik@yandex.ru>
2026-06-18 13:39:37 +0300

    Исправляет тест жанров

--- tests/Feature/GenreApiTest.php
+++ tests/Feature/GenreApiTest.php
@@ -3,10 +3,15 @@
 namespace Tests\Feature;
 
 use App\Models\Genre;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class GenreApiTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_returns_all_genres_with_200_response(): void
     {
         Genre::factory()->count(3)->create();
@@ -21,18 +26,38 @@ class GenreApiTest extends TestCase
                  ]);
     }
 
-    public function test_patch_genre_changes_name(): void
+    public function test_user_without_moderator_role_cant_patch_genre(): void
     {
         $genre = Genre::factory()->create(['name' => 'Комедия']);
 
-        $response = $this->patchJson('/api/genres/' . $genre->id, ['name' => 'Ужасы']);
+        $unauthorizedResponse = $this->patchJson('/api/genres/' . $genre->id, ['name' => 'Ужасы']);
 
-        $response->assertStatus(200)
-                 ->assertJsonStructure([
+        $unauthorizedResponse->assertStatus(401);
+
+        $user = User::factory()->create();
+
+        $notModeratorResponse = $this->actingAs($user)->patchJson('/api/genres/' . $genre->id, ['name' => 'Ужасы']);
+
+        $notModeratorResponse->assertStatus(403);
+    }
+
+    public function test_moderator_can_patch_genres(): void
+    {
+        $genre = Genre::factory()->create(['name' => 'Комедия']);
+
+        $user = User::factory()->create();
+        $role = Role::create(['name' => 'moderator']);
+
+        $user->roles()->attach($role);
+
+        $response = $this->actingAs($user)->patchJson('/api/genres/' . $genre->id, ['name' => 'Ужасы']);
+
+        $response->assertStatus(200)->assertJson([
                      'data' => [
                          'id' => $genre->id,
                          'name' => 'Ужасы',
                      ],
                  ]);
+        ;
     }
 }
